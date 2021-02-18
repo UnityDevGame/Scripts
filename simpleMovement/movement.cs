85f6f8ab5fbc18a2dd63e37a8d7320aa17824d93
Rafał Gondek <48727746+Tuptuus@users.noreply.github.com>
2021-02-18 01:19:58 +0100

    Add files via upload

---
+++ simpleMovement/movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float xInput;
+    public float zInput;
+
+    public float moveSpeed;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        float xVelocity = xInput * moveSpeed;
+        float zVelocity = zInput * moveSpeed;
+
+        rb.velocity = new Vector3(xVelocity, rb.velocity.y, zVelocity);
+    }
+}
